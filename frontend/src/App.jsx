1ab4f0fe750f03267a5eb68d8310d178da49b135
gigokomrade apps <zetechuniversity254@gmail.com>
2023-12-03 13:55:02 +0000

    server commit url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
     e.preventDefault();
     try {
      
-      const response = await axios.post('https://5000-gigokomrade-safaricomte-cc888bocjja.ws-eu106.gitpod.io/token', {
+      const response = await axios.post('https://safaricom-testnet-server.vercel.app/token', {
         phone,
         amount,
       });
